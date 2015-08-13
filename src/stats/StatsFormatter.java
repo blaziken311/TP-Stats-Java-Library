187552700178ec106af4a6db438309fdc8a95253
Arjun Srivastav <arjun.stv@gmail.com>
2015-08-13 13:58:44 -0400

    Added Documentation

--- src/stats/StatsFormatter.java
+++ src/stats/StatsFormatter.java
@@ -4,6 +4,11 @@ package stats;
  * Created by arjun on 8/10/15.
  */
 public class StatsFormatter {
+    /**
+     * A static method that simply converts seconds to HH:MM:SS
+     * @param seconds The number of seconds to convert to HH:MM:SS
+     * @return The HH:MM:SS representation of the number of seconds passed in.
+     */
     public static String secondsToHours( int seconds ) {
         int hours = (int) (seconds / 3600.0);
         seconds -= hours * 3600;
